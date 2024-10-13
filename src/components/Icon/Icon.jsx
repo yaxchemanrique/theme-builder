def25f6fd6a195bc1268e0195772fd482de5ea9d
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-13 12:38:27 -0700

    fix: svg clip-rule and fill-rule html properties

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -341,6 +341,24 @@ function Icon({ type, theme, name, color = "default" }) {
         </svg>
       );
 
+    case "outline-caretDown":
+      return (
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M16.5303 8.96967C16.8232 9.26256 16.8232 9.73744 16.5303 10.0303L12.5303 14.0303C12.2374 14.3232 11.7626 14.3232 11.4697 14.0303L7.46967 10.0303C7.17678 9.73744 7.17678 9.26256 7.46967 8.96967C7.76256 8.67678 8.23744 8.67678 8.53033 8.96967L12 12.4393L15.4697 8.96967C15.7626 8.67678 16.2374 8.67678 16.5303 8.96967Z"
+            fill={fillColor}
+          />
+        </svg>
+      );
+
     case "solid-google":
       return (
         <svg
@@ -640,6 +658,24 @@ function Icon({ type, theme, name, color = "default" }) {
           />
           <path
             d="M16.5 12C16.5 11.1716 17.1716 10.5 18 10.5C18.8284 10.5 19.5 11.1716 19.5 12C19.5 12.8284 18.8284 13.5 18 13.5C17.1716 13.5 16.5 12.8284 16.5 12Z"
+            fill={fillColor}
+          />
+        </svg>
+      );
+
+    case "solid-caretDown":
+      return (
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M16.5303 8.96967C16.8232 9.26256 16.8232 9.73744 16.5303 10.0303L12.5303 14.0303C12.2374 14.3232 11.7626 14.3232 11.4697 14.0303L7.46967 10.0303C7.17678 9.73744 7.17678 9.26256 7.46967 8.96967C7.76256 8.67678 8.23744 8.67678 8.53033 8.96967L12 12.4393L15.4697 8.96967C15.7626 8.67678 16.2374 8.67678 16.5303 8.96967Z"
             fill={fillColor}
           />
         </svg>
